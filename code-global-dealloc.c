29d48320a43cd2381a72340c1764d784366a7976
midnqp <midnightquantumprogrammer@gmail.com>
2023-04-01 11:20:25 +0600

    fix: resize pointer-holder in global dealloc

--- code-global-dealloc.c
+++ code-global-dealloc.c
@@ -1,32 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
 
-#define ADD(a, b) a b
-#define log(label, ...) \
-  printf(ADD(ADD("\033[104m", label), "\033[0m") __VA_ARGS__)
-
-static void** freeable;
+static void **freeable_list;
 static int freeable_count = 0;
 
 void dealloc() {
   for (int i = 0; i < freeable_count; i++) {
-    if (freeable[i] == NULL) continue;
-    log("dealloc", " address %p\n", freeable[i]);
-    free(freeable[i]);
-    freeable[i] = NULL;
+    if (freeable_list[i] == NULL)
+      continue;
+    free(freeable_list[i]);
+    freeable_list[i] = NULL;
+    freeable_count--;
   }
+  free(freeable_list);
 }
 
-void* alloc(int len) {
-  void* result = malloc(len);
-  log("alloc", " %d bytes\n", len);
-  freeable[freeable_count] = result;
+void *alloc(size_t len) {
+  void *result = malloc(len);
+  // realloc and resize the freeable_list
+  freeable_list = realloc(freeable_list, (freeable_count + 1) * sizeof(void *));
+  freeable_list[freeable_count] = result;
   freeable_count++;
   return result;
 };
 
-#include <stdio.h>
-#include <string.h>
-
+/*
 #ifdef free
 #undef free
 #define free(x) \
@@ -40,6 +40,7 @@ void* alloc(int len) {
 #undef malloc
 #define malloc(x) alloc(x)
 #endif
+*/
 
 int main() {
   char* a = alloc(9);
